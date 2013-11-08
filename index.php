cfcfd17693e3b6438ec0ce1971e5fdaec4941bbb
Tobias Rieck <tobias.rieck@benndorf.de>
2013-11-08 23:50:32 +0100

    lbvalus

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="de">
   <head>
     <title>Bootstrap 101 Template - Tobias' Test</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -14,15 +14,30 @@
     <![endif]-->
   </head>
   <body>
-    <h1>Hello, world!</h1>
-        Welcome at Tobias' Site
-        <?php
-        // put your code here
-        phpinfo();
-        ?>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://code.jquery.com/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+        <div class="nav navbar-inverse navbar-fixed-bottom" role="navigation">
+            <ul class="nav navbar-nav">
+                <li><a class="navbar-brand" href="new.html"><img src="img/img.jpg" height="30" ></a></li>
+                <li class="active"><a href="#">Home</a></li>
+            </ul>
+        </div>
+    <div class="container">
+        
+        <div class="page-header">
+            <h1>Hello, world! <small>Welcome at Tobias' Site</small></h1>
+            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+            <script src="https://code.jquery.com/jquery.js"></script>
+            <!-- Include all compiled plugins (below), or include individual files as needed -->
+            <script src="dist/js/bootstrap.min.js"></script>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-md-8">
+                <div class="jumbotron">xs-12 md-8</div>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <div class="jumbotron">xs-6 md-4</div>
+                    
+            </div>
+        </div>
+    </div>
   </body>
 </html>
