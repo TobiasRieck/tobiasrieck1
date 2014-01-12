bb455beeb3d1147df53c0279c9e4164ca93ac73e
Tobias Rieck <tobias.rieck@benndorf.de>
2014-01-12 02:28:53 +0100

    Ohne accessability auf Startseite

--- cloudcontrol.sh
+++ cloudcontrol.sh
@@ -1,3 +1,14 @@
+in Git bash:
 cd Documents/NetBeansProjects/tobiasrieck1/
+
+
+git status
+ggf. git add *
+ggf. git rm dateiname
+git commit -m "Bemerkung"
+git push origin master
+(tobiasrieck) und Standard4
+
+nach CloudControl bringen
 /c/Program\ Files\ \(x86\)/cloudControl/cctrlapp tobiasrieck1 push
 /c/Program\ Files\ \(x86\)/cloudControl/cctrlapp tobiasrieck1 deploy
